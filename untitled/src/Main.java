97174bb972842078fb36769c43cc89b28f7c16ff
DanieliusNev <danieliusneverdauskas@gmail.com>
2022-10-31 15:05:45 +0100

    test

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Heyyy");
+        System.out.println("Heyyy");
     }
+    public class;
 
     public static void Test(){}
 
